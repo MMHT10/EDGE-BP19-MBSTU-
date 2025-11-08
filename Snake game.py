8037cd9d90094a72f242bf5d980d8c546970a366
Maruf Hasan <marufhasan393@gmail.com>
2025-11-09 00:59:17 +0600

    Refactor Snake Game code and update author name

--- Snake game.py
+++ Snake game.py
@@ -17,7 +17,7 @@ dis_width = 600
 dis_height = 400
 
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game by ChatGPT')
+pygame.display.set_caption('Snake Game by MARUF')
 
 clock = pygame.time.Clock()
 
@@ -121,3 +121,4 @@ def gameLoop():
     quit()
 
 gameLoop()
+
